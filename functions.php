58beb5324e2b84f7a3b0ca4202191d57bdf23af7
bobimicroweber <bobi@microweber.com>
2020-09-28 12:18:01 +0300

    update

---
+++ functions.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Bojidar
+ * Date: 9/28/2020
+ * Time: 11:13 AM
+ */
+
+autoload_add_namespace(__DIR__.'/src/', 'MicroweberPackages\\Invoice\\');
---
+++ src/InvoicesManagerServiceProvider.php
@@ -0,0 +1,34 @@
+<?php
+/*
+ * This file is part of the Microweber framework.
+ *
+ * (c) Microweber CMS LTD
+ *
+ * For full license information see
+ * https://github.com/microweber/microweber/blob/master/LICENSE
+ *
+ */
+
+namespace MicroweberPackages\Invoice;
+
+use Illuminate\Support\Facades\View;
+use Illuminate\Support\ServiceProvider;
+
+class InvoicesManagerServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->loadRoutesFrom(__DIR__ . '/routes/admin.php');
+    }
+
+    public function boot()
+    {
+        View::addNamespace('invoice', __DIR__.'/resources/views');
+        $this->loadMigrationsFrom(__DIR__ . '/database/');
+    }
+}
